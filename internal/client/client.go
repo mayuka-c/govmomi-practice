e6ff1b0a7556dd3fc8c4598dc1a122ab22ee1812
mayuka-c <mayuka-c@hpe.com>
2023-04-18 00:41:36 +0530

    Get All VMs

--- internal/client/client.go
+++ internal/client/client.go
@@ -5,9 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net/url"
+	"sort"
+	"strings"
 
 	"github.com/mayuka-c/govmomi-practice/config"
 	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/find"
+	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/property"
+	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/types"
 )
 
 type VCentreClient struct {
@@ -48,4 +55,80 @@ func (vc *VCentreClient) LogoutClient() {
 
 func (vc *VCentreClient) GetVersion() string {
 	return vc.client.ServiceContent.About.Version
+}
+
+func (vc *VCentreClient) GetAllDataCenters() ([]*object.Datacenter, error) {
+	f := find.NewFinder(vc.client.Client, true)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	datacenters, err := f.DatacenterList(ctx, "*")
+	if err != nil {
+		log.Fatalf("Logging in error: %s\n", err.Error())
+		return nil, err
+	}
+
+	log.Println("Datacenters: ", datacenters)
+	return datacenters, nil
+}
+
+type ByName []mo.VirtualMachine
+
+func (n ByName) Len() int           { return len(n) }
+func (n ByName) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+func (n ByName) Less(i, j int) bool { return strings.ToLower(n[i].Name) < strings.ToLower(n[j].Name) }
+
+func (vc *VCentreClient) GetVMs() ([]mo.VirtualMachine, []mo.VirtualMachine) {
+	f := find.NewFinder(vc.client.Client, true)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	dcList, err := vc.GetAllDataCenters()
+	if err != nil {
+		log.Fatalf("Logging in error: %s\n", err.Error())
+	}
+
+	var virtualMachines, vmTemplates []mo.VirtualMachine
+
+	for _, dc := range dcList {
+		// set the current datacenter
+		f.SetDatacenter(dc)
+		// Find all virtual machines in datacenter
+		vms, err := f.VirtualMachineList(ctx, "*")
+		if err != nil {
+			log.Fatalf("Logging in error: %s\n", err.Error())
+		}
+
+		pc := property.DefaultCollector(vc.client.Client)
+
+		// Convert datastores into list of references
+		var refs []types.ManagedObjectReference
+		for _, vm := range vms {
+			refs = append(refs, vm.Reference())
+		}
+		// Retrieve name property for all vms
+		var vmt []mo.VirtualMachine
+		err = pc.Retrieve(ctx, refs, nil, &vmt)
+		if err != nil {
+			log.Fatalf("Logging in error: %s\n", err.Error())
+		}
+
+		fmt.Println("Virtual machines found:", len(vmt))
+		sort.Sort(ByName(vmt))
+		for _, vm := range vmt {
+			if vm.Config == nil {
+				log.Println("Config is nil")
+				continue
+			}
+			if vm.Config.Template {
+				vmTemplates = append(vmTemplates, vm)
+			} else {
+				virtualMachines = append(virtualMachines, vm)
+			}
+		}
+	}
+
+	return virtualMachines, vmTemplates
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/mayuka-c/govmomi-practice/config"
@@ -19,4 +20,19 @@ func main() {
 	client := client.NewVCentreClient(serviceConfig)
 
 	log.Printf("VCenter Version: %s", client.GetVersion())
+
+	client.GetAllDataCenters()
+
+	virtualMachines, vmTemplates := client.GetVMs()
+	log.Printf("Virtual Machine Count: %d", len(virtualMachines))
+	fmt.Println("----------------------------")
+	for _, vm := range virtualMachines {
+		fmt.Println(vm.Name)
+	}
+	fmt.Printf("\n\n")
+	log.Printf("VM Templates Count: %d", len(vmTemplates))
+	fmt.Println("----------------------------")
+	for _, vmTemplate := range vmTemplates {
+		fmt.Println(vmTemplate.Name)
+	}
 }
